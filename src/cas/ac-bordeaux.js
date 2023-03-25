df91987da758b3214542efb56e1f121604a10743
Galitan-dev <galitan.dev@gmail.com>
2023-03-25 11:45:07 +0100

    feat: lycee connectee is finally working

--- src/cas/ac-bordeaux.js
+++ src/cas/ac-bordeaux.js
@@ -1,11 +1,43 @@
-const aten = require('./generics/aten');
+const jsdom = require('jsdom');
+const axioRequest = require('../axioRequest');
 
-module.exports = (url, account, username, password) => aten.login({
-    url,
-    account,
-    username,
-    password,
+const { getDOM, submitForm, extractStart } = require('./api');
+const { errors } = require('../errors');
 
-    startURL: '/sso/SSO?SPEntityID=https%3A%2F%2Fmon.lyceeconnecte.fr%2Fauth%2Fsaml%2Fmetadata%2Fidp.xml',
-    atenURL: 'bv.ac-bordeaux.fr'
-});
+async function login(url, account, username, password)
+{
+    const jar = new jsdom.CookieJar();
+    const res = await axioRequest({
+        url: 'https://educonnect.education.gouv.fr/idp/profile/SAML2/Unsolicited/SSO?providerId=' +
+            'https%3A%2F%2Fmon.lyceeconnecte.fr%2Fauth%2Fsaml%2Fmetadata%2Fidp.xml',
+        method: 'GET',
+        jar
+    });
+
+    let dom = await getDOM({
+        url: 'https://educonnect.education.gouv.fr/idp/profile/SAML2/Redirect/SSO?' +
+            res.headers.location.split('?')[1],
+        method: 'POST',
+        jar,
+        data: {
+            'j_username': username,
+            'j_password': password,
+            '_eventId_proceed': ''
+        },
+        followRedirects: true
+    });
+
+    if (!dom.window.document.querySelector('input[name=SAMLResponse]')) {
+        throw errors.WRONG_CREDENTIALS.drop();
+    }
+
+    dom = await submitForm({ dom, jar, followRedirects: false });
+
+    return extractStart(await getDOM({
+        url: `${url}${account.value}.html`,
+        jar,
+        asIs: true
+    }));
+}
+
+module.exports = login;
